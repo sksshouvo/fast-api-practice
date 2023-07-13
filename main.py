745fb61eb19db48dd5e73d5bafafa221a436d7f4
salmankabirptl21 <salmankabirptl21@gmail.com>
2023-07-13 13:25:11 +0600

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
 
-my_awesome_api  = FastAPI()
+app = FastAPI()
 
-@my_awesome_api.get("/")
+@app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-@my_awesome_api.get("/items/{item_id}")
+@app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
